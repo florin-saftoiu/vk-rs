e4b8afbe8aff198fe5132a3317580a53aa85e433
Florin Saftoiu <florin.saftoiu@soft-to-you.com>
2021-12-14 10:47:55 +0200

    winit

---
+++ src/main.rs
@@ -0,0 +1,23 @@
+#![windows_subsystem = "windows"]
+
+use winit::{
+    event::{WindowEvent, Event},
+    event_loop::{ControlFlow, EventLoop},
+    window::WindowBuilder,
+};
+fn main() {
+    let event_loop = EventLoop::new();
+    let window = WindowBuilder::new().build(&event_loop).unwrap();
+
+    event_loop.run(move |event, _, control_flow| {
+        *control_flow = ControlFlow::Wait;
+
+        match event {
+            Event::WindowEvent {
+                event: WindowEvent::CloseRequested,
+                window_id,
+            } if window_id == window.id() => *control_flow = ControlFlow::Exit,
+            _ => (),
+        }
+    });
+}
