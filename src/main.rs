3670217350c40fb90cd2e2493d5f8f5fcdb40fb8
Florin Saftoiu <florin.saftoiu@soft-to-you.com>
2021-12-14 13:01:37 +0200

    customize winit window

--- src/main.rs
+++ src/main.rs
@@ -1,22 +1,40 @@
 #![windows_subsystem = "windows"]
 
 use winit::{
-    event::{WindowEvent, Event},
+    dpi::LogicalSize,
+    event::{ElementState, Event, KeyboardInput, VirtualKeyCode, WindowEvent},
     event_loop::{ControlFlow, EventLoop},
     window::WindowBuilder,
 };
 fn main() {
     let event_loop = EventLoop::new();
-    let window = WindowBuilder::new().build(&event_loop).unwrap();
+    let _window = WindowBuilder::new()
+        .with_title("vk-rs")
+        .with_inner_size(LogicalSize::new(800, 600))
+        .build(&event_loop)
+        .unwrap();
 
     event_loop.run(move |event, _, control_flow| {
         *control_flow = ControlFlow::Wait;
 
         match event {
-            Event::WindowEvent {
-                event: WindowEvent::CloseRequested,
-                window_id,
-            } if window_id == window.id() => *control_flow = ControlFlow::Exit,
+            Event::WindowEvent { event, .. } => match event {
+                WindowEvent::CloseRequested => *control_flow = ControlFlow::Exit,
+                WindowEvent::KeyboardInput { input, .. } => match input {
+                    KeyboardInput {
+                        state,
+                        virtual_keycode,
+                        ..
+                    } => match (state, virtual_keycode) {
+                        (ElementState::Pressed, Some(VirtualKeyCode::Escape)) => {
+                            dbg!();
+                            *control_flow = ControlFlow::Exit
+                        }
+                        _ => (),
+                    },
+                },
+                _ => (),
+            },
             _ => (),
         }
     });
