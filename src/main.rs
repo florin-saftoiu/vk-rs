65ec9f02c86f9db1369ec0769d2601ca5fd2cf42
Florin Saftoiu <florin.saftoiu@soft-to-you.com>
2021-12-30 10:06:52 +0200

    instance creation

--- src/main.rs
+++ src/main.rs
@@ -1,25 +1,44 @@
 #![windows_subsystem = "windows"]
 
+use std::error::Error;
+
+use ash::{vk, Entry};
 use winit::{
     dpi::LogicalSize,
     event::{ElementState, Event, KeyboardInput, VirtualKeyCode, WindowEvent},
     event_loop::{ControlFlow, EventLoop},
     window::WindowBuilder,
 };
-fn main() {
+
+fn main() -> Result<(), Box<dyn Error>> {
     let event_loop = EventLoop::new();
     let _window = WindowBuilder::new()
         .with_title("vk-rs")
         .with_inner_size(LogicalSize::new(800, 600))
-        .build(&event_loop)
-        .unwrap();
+        .build(&event_loop)?;
+
+    // Ash loads Vulkan dynamically, ash::Entry is the library loader and the entrypoint into the Vulkan API.
+    // In the future, Ash should also support loading Vulkan as a static library.
+    let entry = unsafe { Entry::new() }?;
+    let app_info = vk::ApplicationInfo {
+        api_version: vk::make_api_version(0, 1, 0, 0),
+        ..Default::default()
+    };
+    let create_info = vk::InstanceCreateInfo {
+        p_application_info: &app_info,
+        ..Default::default()
+    };
+    let instance = unsafe { entry.create_instance(&create_info, None) }?;
 
     event_loop.run(move |event, _, control_flow| {
-        *control_flow = ControlFlow::Wait;
+        *control_flow = ControlFlow::Poll;
 
         match event {
             Event::WindowEvent { event, .. } => match event {
-                WindowEvent::CloseRequested => *control_flow = ControlFlow::Exit,
+                WindowEvent::CloseRequested => {
+                    unsafe { instance.destroy_instance(None) };
+                    *control_flow = ControlFlow::Exit
+                }
                 WindowEvent::KeyboardInput { input, .. } => match input {
                     KeyboardInput {
                         state,
@@ -27,7 +46,7 @@ fn main() {
                         ..
                     } => match (state, virtual_keycode) {
                         (ElementState::Pressed, Some(VirtualKeyCode::Escape)) => {
-                            dbg!();
+                            unsafe { instance.destroy_instance(None) };
                             *control_flow = ControlFlow::Exit
                         }
                         _ => (),
@@ -35,6 +54,7 @@ fn main() {
                 },
                 _ => (),
             },
+            Event::MainEventsCleared => {}
             _ => (),
         }
     });
