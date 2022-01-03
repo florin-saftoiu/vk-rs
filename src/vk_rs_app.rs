fd42c388f7c7f02d0c890c0d69ec1d527ec89789
Florin Saftoiu <florin.saftoiu@soft-to-you.com>
2022-01-03 14:15:08 +0200

    add debug messenger

--- src/vk_rs_app.rs
+++ src/vk_rs_app.rs
@@ -1,14 +1,45 @@
 use std::{error::Error, ffi::CStr};
 #[cfg(debug_assertions)]
-use std::ffi::CString;
+use std::ffi::{CString, c_void};
 
 use ash::{vk, Entry, Instance};
 #[cfg(debug_assertions)]
-use ash::extensions::ext::DebugUtils;
+use ash::{extensions::ext::DebugUtils, vk::DebugUtilsMessengerEXT};
 
 pub struct VkRsApp {
     _entry: Entry,
-    instance: Instance
+    instance: Instance,
+    #[cfg(debug_assertions)]
+    debug_utils: Option<(DebugUtils, DebugUtilsMessengerEXT)>,
+}
+
+#[cfg(debug_assertions)]
+unsafe extern "system" fn vk_debug_utils_callback (
+    message_severity: vk::DebugUtilsMessageSeverityFlagsEXT,
+    message_type: vk::DebugUtilsMessageTypeFlagsEXT,
+    p_callback_data: *const vk::DebugUtilsMessengerCallbackDataEXT,
+    _p_user_data: *mut c_void
+) -> vk::Bool32 {
+    let msg_severity = match message_severity {
+        vk::DebugUtilsMessageSeverityFlagsEXT::VERBOSE => "[VERBOSE]",
+        vk::DebugUtilsMessageSeverityFlagsEXT::INFO => "[INFO]",
+        vk::DebugUtilsMessageSeverityFlagsEXT::WARNING => "[WARNING]",
+        vk::DebugUtilsMessageSeverityFlagsEXT::ERROR => "[ERROR]",
+        _ => "[UNKNOWN_SEVERITY]"
+    };
+
+    let msg_type = match message_type {
+        vk::DebugUtilsMessageTypeFlagsEXT::GENERAL => "[GENERAL]",
+        vk::DebugUtilsMessageTypeFlagsEXT::PERFORMANCE => "[PERFORMANCE]",
+        vk::DebugUtilsMessageTypeFlagsEXT::VALIDATION => "[VALIDATION]",
+        _ => "[UNKNOWN_TYPE]"
+    };
+
+    let msg = CStr::from_ptr((*p_callback_data).p_message);
+    
+    println!("{} {} {:?}", msg_severity, msg_type, msg);
+
+    vk::FALSE
 }
 
 impl VkRsApp {
@@ -56,43 +87,71 @@ impl VkRsApp {
         
         if enable_validation_layers {
             println!("Validation layers available.");
-        } else {
-            println!("Validation layers not available.");
-        }
 
-        if enable_validation_layers {
             required_extensions.push(DebugUtils::name());
-        }
+            let p_enabled_extension_names = required_extensions.iter().map(|e| e.as_ptr()).collect::<Vec<*const i8>>();
 
-        let enabled_layer_names = validation_layers.iter().map(|l| CString::new(*l).unwrap()).collect::<Vec<CString>>();
-        let p_enabled_layer_names = enabled_layer_names.iter().map(|l| l.as_ptr()).collect::<Vec<*const i8>>();
-        let p_enabled_extension_names = required_extensions.iter().map(|e| e.as_ptr()).collect::<Vec<*const i8>>();
-        let create_info = if enable_validation_layers {
-            vk::InstanceCreateInfo {
+            let enabled_layer_names = validation_layers.iter().map(|l| CString::new(*l).unwrap()).collect::<Vec<CString>>();
+            let p_enabled_layer_names = enabled_layer_names.iter().map(|l| l.as_ptr()).collect::<Vec<*const i8>>();
+
+            let instance_debug_utils_messenger_create_info = vk::DebugUtilsMessengerCreateInfoEXT {
+                message_severity: vk::DebugUtilsMessageSeverityFlagsEXT::all(),
+                message_type: vk::DebugUtilsMessageTypeFlagsEXT::all(),
+                pfn_user_callback: Some(vk_debug_utils_callback),
+                ..Default::default()
+            };
+
+            let create_info = vk::InstanceCreateInfo {
+                p_next: &instance_debug_utils_messenger_create_info as *const vk::DebugUtilsMessengerCreateInfoEXT as *const c_void,
                 p_application_info: &app_info,
                 enabled_layer_count: validation_layers.len() as u32,
                 pp_enabled_layer_names: p_enabled_layer_names.as_ptr(),
                 enabled_extension_count: required_extensions.len() as u32,
                 pp_enabled_extension_names: p_enabled_extension_names.as_ptr(),
                 ..Default::default()
-            }
+            };
+
+            let instance = unsafe { entry.create_instance(&create_info, None) }?;
+        
+            println!("Vulkan instance created.");
+
+            let debug_utils_loader = DebugUtils::new(&entry, &instance);
+            let messenger_create_info = vk::DebugUtilsMessengerCreateInfoEXT {
+                message_severity: vk::DebugUtilsMessageSeverityFlagsEXT::all(),
+                message_type: vk::DebugUtilsMessageTypeFlagsEXT::all(),
+                pfn_user_callback: Some(vk_debug_utils_callback),
+                ..Default::default()
+            };
+            let debug_utils_messenger = unsafe { debug_utils_loader.create_debug_utils_messenger(&messenger_create_info, None) }?;
+
+            return Ok(VkRsApp {
+                // The entry has to live as long as the app, otherwise you get an access violation when destroying instance.
+                _entry: entry,
+                instance,
+                debug_utils: Some((debug_utils_loader, debug_utils_messenger))
+            })
         } else {
-            vk::InstanceCreateInfo {
+            println!("Validation layers not available.");
+
+            let p_enabled_extension_names = required_extensions.iter().map(|e| e.as_ptr()).collect::<Vec<*const i8>>();
+
+            let create_info = vk::InstanceCreateInfo {
                 p_application_info: &app_info,
                 enabled_extension_count: required_extensions.len() as u32,
                 pp_enabled_extension_names: p_enabled_extension_names.as_ptr(),
                 ..Default::default()
-            }
-        };
-        let instance = unsafe { entry.create_instance(&create_info, None) }?;
-
-        println!("Vulkan instance created.");
-
-        Ok(VkRsApp {
-            // The entry has to live as long as the app, otherwise you get an access violation when destroying instance.
-            _entry: entry,
-            instance
-        })
+            };
+            let instance = unsafe { entry.create_instance(&create_info, None) }?;
+    
+            println!("Vulkan instance created.");
+    
+            return Ok(VkRsApp {
+                // The entry has to live as long as the app, otherwise you get an access violation when destroying instance.
+                _entry: entry,
+                instance,
+                debug_utils: None
+            })
+        }
     }
 
     #[cfg(not(debug_assertions))]
@@ -130,6 +189,11 @@ impl VkRsApp {
 
 impl Drop for VkRsApp {
     fn drop(&mut self) {
+        #[cfg(debug_assertions)]
+        if let Some((debug_utils_loader, debug_utils_messenger)) = &self.debug_utils {
+            unsafe { debug_utils_loader.destroy_debug_utils_messenger(*debug_utils_messenger, None) };
+        }
+
         // The ash::Entry used to create the instance has to be alive when calling ash::Instance::destroy_instance.
         unsafe { self.instance.destroy_instance(None) };
         #[cfg(debug_assertions)]
